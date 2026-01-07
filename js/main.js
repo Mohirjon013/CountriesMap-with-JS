9fff960b8eb69cc603db2cc167ba13716d7e9ce6
Mohirjon013 <mohirjon26@gmail.com>
2026-01-07 17:38:11 +0500

    changed some parts and finished fisrt step

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let elHeroList = document.querySelector(".hero__list")
+elHeroList.className = "flex flex-wrap gap-y-[40px] justify-between"
+let elInputSearch = document.querySelector(".input-search")
 
 
 const countries = [
@@ -78,11 +80,33 @@ function renderCountry(arr){
     elHeroList.innerHTML = null
     arr.forEach(item => {
         let elHeroItem = document.createElement("li")
-        elHeroItem.className = "w-[300px] bg-slate-400"
+        elHeroItem.className = "w-[330px] p-5 bg-slate-200  border border-slate-500 rounded-[10px]"
         elHeroItem.innerHTML = `
-            <img src=${item.flag} alt="flags" width="100" height="100">
+            <img class="object-cover w-full h-[200px] rounded-[10px]" src=${item.flag} alt="flags" width="200" height="200">
+            <h2 class="font-bold text-[20px] text-zinc-600 mt-[15px]">Country: ${item.name}</h2>
+            <p class="font-normal text-[18px] text-slate-700">Capital: ${item.capital}</p>
+            <p class="font-normal text-[18px] text-slate-700">Population: ${item.population}</p>
+        
+            <ul class="w-[200px] flex items-center gap-[10px] mt-[20px]">
+                <li class="p-2 bg-slate-400 rounded-[10px] hover:scale-125 duration-300 cursor-pointer">
+                    <img class="" src="./images/like-icon.svg" alt="like-icon" width="35" height="35">
+                </li>
+                <li class="p-2 bg-slate-400 rounded-[10px] hover:scale-125 duration-300 cursor-pointer">
+                    <img class="" src="./images/saved-icon.svg" alt="saved-icon" width="35" height="35">
+                </li>
+                <li class="p-2 bg-slate-400 rounded-[10px] hover:scale-125 duration-300 cursor-pointer">
+                    <img class="" src="./images/more-icon.svg" alt="more-icon" width="35" height="35">
+                </li>
+            </ul>
         `
         elHeroList.appendChild(elHeroItem)
     });
 }
 renderCountry(countries)
+
+elInputSearch.addEventListener("keyup", function(evnt){
+    const searchedValue = evnt.target.value.toLocaleLowerCase()
+    const filteredObj = countries.filter(item => item.name.toLocaleLowerCase().includes(searchedValue))
+
+    renderCountry(filteredObj)
+})
